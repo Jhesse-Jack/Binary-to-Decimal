503a8d1e21d8d4e0e15b138b8054fe0cc9b22b35
Jhesse-Jack <armahjames9@gmail.com>
2021-03-26 11:35:33 +0000

    Addressed everything; Up Next: Address how to reject numbers not between 0 and 1 and structure everthing into components

--- src/App.js
+++ src/App.js
@@ -1,57 +1,73 @@
-import React from 'react';
-import './App.css';
+import React, {useState} from 'react';
 import './bootstrap.min.css';
-import Modal from './components/modal';
+import './App.css';
+import { Button, Modal } from 'react-bootstrap';
 
-class App extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      dec_num: '',
-    };
-    this.handleNumChange = (e) => {
-      this.setState({
-        dec_num: e.target.value,
-      });
+const App = () => {
+    let [decnum, setDecnum] = useState('');
+    function handleNumChange(e){
+      setDecnum(e.target.value);
+      // var numbers = [0-1]
+      // if(decnum.match(numbers)){
+      //   setDecnum(e.target.value)
+      // }
+      // else {
+      //   alert("Your number should be between 0 and 1");
+      // }
     }
-    let sum = 0;
+    let summation = 0;
     let i = 0;
 
-    //the logic behind
-    this.converter = () => {
-      while(this.state.dec_num){
-        sum = sum + Math.pow(2, i) * (this.state.dec_num % 10);
-        i++;
-        this.state.dec_num = parseInt(this.state.dec_num / 10);
-        console.log(sum);
-      }        
-      alert(sum);
-    }
-  }
-
-  render(){
     return (
-      <div className="App">
-        <header className="App-header">
-          <p> Enter any binary number (to be converted to decimal base): 
-            &nbsp;
-            <input 
-              type="number" 
-              className="form-control"
-              placeholder="Enter any binary number"
-              onChange={this.handleNumChange} />
-          </p>
-          <button 
-            type="button" 
-            className="btn btn-light"
-            onClick={this.converter}>
-            {/* //onClick={Modal}> */}
-            CONVERT
-          </button>
-        </header>
-      </div>
+        <div className="App">
+            <header className="App-header">
+                <p> Enter any binary number (to be converted to decimal base): 
+                    &nbsp;
+                    <input 
+                        type="number"
+                        className="form-control"
+                        placeholder="Enter any binary number"
+                        onChange={handleNumChange} />
+                </p>
+                <Modale />
+            </header>
+        </div>
     );
-  }
-}
+
+    function Modale(props){
+      const [show, setShow] = useState(false);
+      const handleShow = () => setShow(true);
+      const handleClose = () => setShow(false);
+      while(decnum){
+        summation = summation + Math.pow(2, i) * (decnum % 10);
+        i++;
+        decnum = parseInt(decnum / 10);
+        console.log(summation);
+      }
+      return (
+        <>
+          <Button variant="primary" onClick={handleShow}>
+            CONVERT
+          </Button>
+    
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title> Binary Converter </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              Your answer is: {summation}
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant = "secondary" onClick={handleClose}>
+                Close
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </>
+      )
+    }
+};
+
+
 
 export default App;
